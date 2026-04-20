27c08ccfafa783ed956bd0460c09a24294895344
spencrr <23708360+spencrr@users.noreply.github.com>
2026-04-20 16:44:11 -0700

    fixup module doc comment

--- rampart/drivers/__init__.py
+++ rampart/drivers/__init__.py
@@ -1,11 +1,7 @@
 # Copyright (c) Microsoft Corporation.
 # Licensed under the MIT license.
 
-"""Driver implementations.
-
-Re-exports StaticDriver and provides the coerce_driver helper
-for ergonomic prompt/driver coercion.
-"""
+"""Driver implementations."""
 
 from rampart.drivers.static import StaticDriver
 
