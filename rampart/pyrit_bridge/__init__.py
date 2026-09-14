c64ad1e8ebaa5071988e7b73efc83835d822b30d
sy0u1ti <110846601+sy0u1ti@users.noreply.github.com>
2026-09-14 19:42:57 +0800

    [FEAT]: Implement PyRIT converter_bridge for PayloadConverter protocol (#69)

--- rampart/pyrit_bridge/__init__.py
+++ rampart/pyrit_bridge/__init__.py
@@ -3,6 +3,10 @@
 
 """PyRIT integration bridge."""
 
+from rampart.pyrit_bridge.converter_bridge import (
+    PyRITConverterBridge,
+    adapt_converter,
+)
 from rampart.pyrit_bridge.llm_bridge import (
     create_prompt_target,
     send_generation_request_async,
@@ -11,6 +15,8 @@ from rampart.pyrit_bridge.llm_bridge import (
 )
 
 __all__ = [
+    "PyRITConverterBridge",
+    "adapt_converter",
     "create_prompt_target",
     "send_generation_request_async",
     "send_judge_request_async",
